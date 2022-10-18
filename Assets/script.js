7d2db7a45202e0c681cdc1a90fedaa5b99e1af5a
Lara Grocke <laragrocke@Laras-MacBook-Air.local>
2022-10-18 20:46:46 +1030

    Added basic country API

--- Assets/script.js
+++ Assets/script.js
@@ -1,15 +1,35 @@
 $(document).ready(function(){
+
+    var countryInputEl = $('#search');
+    var searchBtn = $('#searchbtn');
+
     var API_KEY = "AIzaSyB4n8ZsQ41g-21s8rjJJDYZFSigRF8Qb4U"
     var video = ''
 
     $("form").submit(function (event) {
         event.preventDefault()
 
-        var search = $("#search").find(":selected").val()
+        var search = $("#search").find(":selected").val();
         console.log(search)
+
+        var countryValue = $('#search').find(":selected").data('country');
+
+        // var search = $("#search").find(":selected");
+        // console.log(search)
+
+        // var countryValue = $(this).find(":selected");
+
+        console.log(countryValue);
+
+        countrySearch(countryValue)
         videoSearch(API_KEY,search,5)
+        
     })
+
     function videoSearch(key, search,maxResults) {
+
+        //search + " tourism"
+
         $.get("https://www.googleapis.com/youtube/v3/search?key=" + key + "&type=video&part=snippet&maxResults=" + maxResults + "&q=" + search,function(data){
             console.log(data)
             data.items.forEach(item => {
@@ -21,6 +41,16 @@ $(document).ready(function(){
             });
         })
     }
+
+    function countrySearch(countryValue) {
+
+        console.log('test');
+
+        var countryURL = `https://restcountries.com/v3.1/name/${countryValue}?fullText=true`;
+        console.log(countryURL);
+
+    }
+    
 
 
 })
